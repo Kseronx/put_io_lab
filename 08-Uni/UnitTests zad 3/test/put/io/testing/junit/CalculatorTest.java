4e61375811170267507daabbbc96612e63bba622
Kseronx <124221116+Kseronx@users.noreply.github.com>
2023-12-07 17:12:34 +0200

    Add files via upload

---
+++ 08-Uni/UnitTests zad 3/test/put/io/testing/junit/CalculatorTest.java
@@ -0,0 +1,40 @@
+package put.io.testing.junit;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    private static Calculator calculator;
+
+    @BeforeEach
+    void prepare(){
+        calculator = new Calculator();
+    }
+
+
+    @Test
+    void testAdd() {
+        assertEquals(4, calculator.add(2, 2));
+        assertEquals(-4, calculator.add(-2, -2));
+        assertEquals(0, calculator.add(-2, 2));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(6, calculator.multiply(2, 3));
+        assertEquals(-4, calculator.multiply(-2, 2));
+        assertEquals(0, calculator.multiply(0, 2));
+    }
+
+
+    @Test
+    void testAddPositiveNumbers(){
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+            calculator.addPositiveNumbers(-2, 3);
+        });
+        assertEquals("The arguments must be positive", thrown.getMessage());
+    }
+}
